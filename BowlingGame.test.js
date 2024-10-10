6e9c047527fe4d8d846100faafcaac66a26633b2
Chris Raible <chris@ghost.org>
2024-10-10 10:03:31 +0100

    Added Game class

---
+++ BowlingGame.test.js
@@ -0,0 +1,8 @@
+const assert = require('node:assert');
+const Game = require('./Game');
+
+describe('BowlingGame', () => {
+    it('testGutterGame', () => {
+        const game = new Game();
+    });
+});
