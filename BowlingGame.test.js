d1d7faf62b546b1aa47a74345837409fcde2762c
Princi Vershwal <vershwal.princi@gmail.com>
2024-10-10 10:18:06 +0100

    Added initial tests and functions

--- BowlingGame.test.js
+++ BowlingGame.test.js
@@ -1,8 +1,32 @@
 const assert = require('node:assert');
 const Game = require('./Game');
+const { before } = require('mocha');
 
 describe('BowlingGame', () => {
-    it('testGutterGame', () => {
-        const game = new Game();
+    let game;
+    before(() => {
+        game = new Game();
+    });
+
+    function rollMany(n, pins) {
+        for (let i=0; i<n; i++) {
+            game.roll(pins);
+        }
+    }
+
+    it('Tesing zero scores', () => {
+        const n = 20;
+        const pins = 0;
+        
+        rollMany(n,pins);
+        assert.strictEqual(game.score(),0);
+    });
+
+    it('Tesing one scores', () => {
+        const n = 20;
+        const pins = 1;
+        
+        rollMany(n,pins);
+        assert.strictEqual(game.score(),20);
     });
 });
--- Game.js
+++ Game.js
@@ -1,5 +1,16 @@
 class Game {
 
+    totalScore = 0;
+
+    roll = (pins) => {
+        this.totalScore += pins;
+        return;
+    }
+
+    score = () => {
+        return this.totalScore;
+    }
+
 }
 
 module.exports = Game;
