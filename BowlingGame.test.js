e4ddbd11b7f60d5378c4c0a6a86548942482221d
Chris Raible <chris@ghost.org>
2024-10-10 10:03:47 +0100

    Revert "Added Game class"
    This reverts commit 6e9c047527fe4d8d846100faafcaac66a26633b2.

--- BowlingGame.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('node:assert');
-const Game = require('./Game');
-
-describe('BowlingGame', () => {
-    it('testGutterGame', () => {
-        const game = new Game();
-    });
-});
